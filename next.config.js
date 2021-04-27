6762f43e00be7ca69ebeb91c523d2d8b67fe74c5
Navdeep Singh <navdeep.er@gmail.com>
2021-04-27 14:17:42 +0530

    Fix vercel deploy

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   env: {
     API_URL: process.env.API_URL,
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+
+    // Important: return the modified config
+    return config;
+  },
 };
