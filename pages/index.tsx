222d4e0f29693e7b77afddb2755209e220b919b7
Navdeep Singh <navdeep.er@gmail.com>
2021-04-27 15:49:16 +0530

    Fix vercel deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,22 @@ const Home = () => (
     </main>
 
     <footer>      
-        Developed by Navdeep Singh
+      Developed by <a href="https://navdeepsingh.in">Navdeep Singh</a>
     </footer>
 
     <ToastContainer />
 
     <style>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
       .container {
         min-height: 100vh;
         padding: 0 0.5rem;
@@ -51,21 +61,10 @@ const Home = () => (
         width: 100%;
         height: 100px;
         border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        text-align: center;
       }
 
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
+     
       a {
         color: inherit;
         text-decoration: none;
@@ -96,44 +95,6 @@ const Home = () => (
       .description {
         line-height: 1.5;
         font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-      }
-
-      * {
-        box-sizing: border-box;
       }
     `}</style>
   </div>
